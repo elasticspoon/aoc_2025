693a0dec9d0d36eadc52e4a15212615bb4a6d6a2
elasticspoon <mail@yuribocharov.dev>
2025-12-28 23:50:10 -0500

    feat: day 7 p2

--- src/bin/day_7.rs
+++ src/bin/day_7.rs
@@ -1,8 +1,26 @@
-use std::{collections::HashSet, fs::read_to_string};
+use std::{collections::HashMap, fs::read_to_string};
 
 fn main() {
     let input = read_to_string("input/day7.txt").expect("Should have been able to read file");
     println!("part 1: {}", count_splits(&input));
+    println!("part 2: {}", count_timelines(&input));
+}
+
+fn count_timelines(input: &str) -> usize {
+    let start = input
+        .lines()
+        .next()
+        .unwrap()
+        .find("S")
+        .expect("missing S in first line");
+
+    let mut lasers = HashMap::from([(start, 1)]);
+    for row in input.lines().skip(1) {
+        let (_, set) = split_lasers(lasers, row);
+        lasers = set;
+    }
+
+    lasers.values().sum()
 }
 
 fn count_splits(input: &str) -> usize {
@@ -14,7 +32,7 @@ fn count_splits(input: &str) -> usize {
         .expect("missing S in first line");
 
     let mut split_count = 0;
-    let mut lasers = HashSet::from([start]);
+    let mut lasers = HashMap::from([(start, 1)]);
     for row in input.lines().skip(1) {
         let (count, set) = split_lasers(lasers, row);
         lasers = set;
@@ -24,41 +42,64 @@ fn count_splits(input: &str) -> usize {
     split_count
 }
 
-fn split_lasers(lasers: HashSet<usize>, row: &str) -> (usize, HashSet<usize>) {
-    let result = HashSet::new();
+fn split_lasers(lasers: HashMap<usize, usize>, row: &str) -> (usize, HashMap<usize, usize>) {
+    let result = HashMap::new();
 
-    lasers
-        .iter()
-        .fold((0, result), |(mut split_count, mut set), &val| {
+    lasers.into_iter().fold(
+        (0, result),
+        |(mut split_count, mut set), (val, timeline_count)| {
             if let Some(char) = row.get(val..=val) {
                 match char {
-                    "." => set.insert(val),
+                    "." => {
+                        set.entry(val)
+                            .and_modify(|v| *v += timeline_count)
+                            .or_insert(timeline_count);
+                    }
                     "^" => {
                         split_count += 1;
-                        set.insert(val - 1);
-                        set.insert(val + 1)
+                        set.entry(val - 1)
+                            .and_modify(|v| *v += timeline_count)
+                            .or_insert(timeline_count);
+                        set.entry(val + 1)
+                            .and_modify(|v| *v += timeline_count)
+                            .or_insert(timeline_count);
                     }
                     _ => panic!("invalid char: {char}"),
                 };
             }
             (split_count, set)
-        })
+        },
+    )
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
-    fn test_split_lasers() {
-        let lasers = HashSet::from([1]);
+    fn test_split_lasers_basic() {
+        let lasers = HashMap::from([(1, 1)]);
         let input = ".^.";
 
-        let want_set = HashSet::from([0, 2]);
+        let want_map = HashMap::from([(0, 1), (2, 1)]);
 
         let (got_count, got_set) = split_lasers(lasers, input);
 
-        assert_eq!(got_set, want_set);
+        assert_eq!(got_set, want_map);
+        assert_eq!(got_count, 1);
+    }
+
+    #[test]
+    fn test_split_lasers_overlaps() {
+        let lasers = HashMap::from([(0, 1), (1, 2)]);
+        let input = ".^.";
+
+        let want_map = HashMap::from([(0, 3), (2, 2)]);
+
+        let (got_count, got_set) = split_lasers(lasers, input);
+
+        assert_eq!(got_set, want_map);
         assert_eq!(got_count, 1);
     }
 
@@ -82,5 +123,27 @@ mod tests {
 ...............";
 
         assert_eq!(count_splits(input), 21);
+    }
+
+    #[test]
+    fn test_count_timelines() {
+        let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+
+        assert_eq!(count_timelines(input), 40);
     }
 }
